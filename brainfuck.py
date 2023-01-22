d59c35025e467a57601cc9c9b54a0e557e019e98
EthanDenny <ethandenny@protonmail.com>
2023-01-22 01:30:43 -0330

    Major refactor
    Split the code into simpler individual functions

--- brainfuck.py
+++ brainfuck.py
@@ -1,55 +1,66 @@
 import sys
 
-cells = [0]
-pointer = 0
-code = ''
-code_pointer = 0
+
+def execute(code: str):
+    cells = [0]
+    mem_ptr = 0
+    code_ptr = 0
+
+    while code_ptr < len(code):
+        match code[code_ptr]:
+            case '<':
+                if mem_ptr > 0: mem_ptr -= 1
+            case '>':
+                mem_ptr += 1
+                if mem_ptr == len(cells): cells.append(0)
+            case '+':
+                cells[mem_ptr] += 1
+                if cells[mem_ptr] == 256: cells[mem_ptr] = 0
+            case '-':
+                cells[mem_ptr] -= 1
+                if cells[mem_ptr] == -1: cells[mem_ptr] = 255
+            case ',':
+                cells[mem_ptr] = ord(input(': '))
+            case '.':
+                print(chr(cells[mem_ptr]), end='')
+            
+            case '[':
+                if cells[mem_ptr] == 0:
+                    unmatched_count = 1
+                    while unmatched_count > 0:
+                        code_ptr += 1
+                        if code[code_ptr] == '[': unmatched_count += 1
+                        if code[code_ptr] == ']': unmatched_count -= 1
+            case ']':
+                if cells[mem_ptr] != 0:
+                    unmatched_count = 1
+                    while unmatched_count > 0:
+                        code_ptr -= 1
+                        if code[code_ptr] == ']': unmatched_count += 1
+                        if code[code_ptr] == '[': unmatched_count -= 1
+            
+        code_ptr += 1
 
 
-if len(sys.argv) > 1:
-    filename = sys.argv[1]
-else:
-    filename = input('File name: ')
+def parse_file(filepath: str):
+    code = ''
 
-with open(filename) as f:
-    for c in f.read():
-        if c in ['<', '>', '+', '-', ',', '.', '[', ']']:
-            code += c
+    with open(filepath) as f:
+        for c in f.read():
+            if c in {'<', '>', '+', '-', ',', '.', '[', ']'}:
+                code += c
+    
+    execute(code)
 
-def get_curr_command():
-    return code[code_pointer]
 
-while code_pointer < len(code):
-    match get_curr_command():
-        case '<':
-            if pointer > 0: pointer -= 1
-        case '>':
-            pointer += 1
-            if pointer == len(cells): cells.append(0)
-        case '+':
-            cells[pointer] += 1
-            if cells[pointer] == 256: cells[pointer] = 0
-        case '-':
-            cells[pointer] -= 1
-            if cells[pointer] == -1: cells[pointer] = 255
-        case ',':
-            cells[pointer] = ord(input(': '))
-        case '.':
-            print(chr(cells[pointer]), end='')
-        
-        case '[':
-            if cells[pointer] == 0:
-                unmatched_count = 1
-                while unmatched_count > 0:
-                    code_pointer += 1
-                    if get_curr_command() == '[': unmatched_count += 1
-                    if get_curr_command() == ']': unmatched_count -= 1
-        case ']':
-            if cells[pointer] != 0:
-                unmatched_count = 1
-                while unmatched_count > 0:
-                    code_pointer -= 1
-                    if get_curr_command() == ']': unmatched_count += 1
-                    if get_curr_command() == '[': unmatched_count -= 1
-        
-    code_pointer += 1
+def main():
+    if len(sys.argv) > 1:
+        filepath = sys.argv[1]
+    else:
+        filepath = input('File name: ')
+    
+    parse_file(filepath)
+
+
+if __name__ == '__main__':
+    main()
